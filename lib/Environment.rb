413863b51ccff9ace6690737234d3e18f296c6db
Adjoa Ntoso <adjoaan@gmail.com>
2019-08-07 00:58:57 +0000

    Removed Medication_List.rb folder & edited Environment.rb.

--- lib/Environment.rb
+++ lib/Environment.rb
@@ -1,7 +1,15 @@
-require_relative "./Medication_List/version"
+require 'pry'
+require 'nokigiri'
+require 'open-url'
+require 'colorize'
+
+require_relative "./version"
+require_relative "./Medicine"
+require_relative "./CLI"
+require_relative "./Scraper"
+require_relative "./version"
 
 module MedicationList
   class Error < StandardError; end
   # Your code goes here...
 end
-require_relative "./CLI"
--- lib/Medication_List.rb
+++
@@ -1,9 +0,0 @@
-require 'pry'
-require 'nokigiri'
-require 'open-url'
-require 'colorize'
-
-require_relative "./Medication_List/version"
-require_relative "./Medication_List/Medicine"
-require_relative "./Medication_List/CLI"
-require_relative "./Medication_List/Scraper"
